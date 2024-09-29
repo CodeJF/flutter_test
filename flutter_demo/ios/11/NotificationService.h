9eb6684d1b29173d500724be2cc6100d1fa39431
jianFeng Xu <xjf@naratech.cn>
2024-09-29 15:00:34 +0800

    a commit

---
+++ flutter_demo/ios/11/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  11
+//
+//  Created by jianFeng Xu on 2024/9/29.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
