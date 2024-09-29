316354c1ce207005552bb44ae54093304e4ef23e
jianFeng Xu <xjf@naratech.cn>
2024-09-29 14:25:56 +0800

    b commit

--- flutter_demo/ios/Runner/AppDelegate.m
+++ flutter_demo/ios/Runner/AppDelegate.m
@@ -7,8 +7,12 @@
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
+
     
     NSLog(@"hello world");
+
+    NSLog(@"你好世界!");
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
